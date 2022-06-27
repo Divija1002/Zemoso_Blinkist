0c24ef5c07c659066ad480bd022239568c4cec48
Divija1002 <divija.amaram@gmail.com>
2022-06-27 11:43:23 +0530

    third commit

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import Theme from "../src/Theme/ThemeFile";
+import { ThemeProvider } from "@mui/material";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +9,13 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+  layout: "none",
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={Theme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
